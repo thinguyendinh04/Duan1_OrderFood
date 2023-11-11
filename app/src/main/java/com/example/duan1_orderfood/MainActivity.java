9721f27b99325e6e817be178dae6e6add7af22d0
Thinguyen04 <inuyasha4002@gmail.com>
2023-11-11 21:04:26 +0700

    Dinh Thi Coder

--- app/src/main/java/com/example/duan1_orderfood/MainActivity.java
+++ app/src/main/java/com/example/duan1_orderfood/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.duan1_orderfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    BottomNavigationView bot_nav;
+    NavigationView nav_view;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
