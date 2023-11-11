cdccd6709898c7aa601f1ada9aa6fc100ee5dd3e
Thinguyen04 <inuyasha4002@gmail.com>
2023-11-11 21:19:39 +0700

    khiem cmt

--- app/src/main/java/com/example/duan1_orderfood/MainActivity.java
+++ app/src/main/java/com/example/duan1_orderfood/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // khiemcmt
     }
 }
